29c1711f0354ec667599419e769174930f6cf0dd
RonnieJ507 <RonnieJ507@gmail.com>
2022-11-21 20:53:19 -0600

    Update trashCan.js

--- root/graphics/models/trashCan.js
+++ root/graphics/models/trashCan.js
@@ -1,110 +1,116 @@
-class TrashCan {
-    #vertexCount;
-    #translationMatrix;
-    #vertices;
-    #trashCanPoints;
+var vertices=[];
+var numVertTrashCan =  0;
 
-    constructor(translationMatrix = mat4()) {
-        this.#vertexCount = 0;
-        this.#translationMatrix = translationMatrix;
-        this.#vertices = [];
-        this.#trashCanPoints = [];
-        this.#IntialPoints();
-    }
+//Sets up the vertices array so the trashCan can be drawn
+function Generate()
+{
+    var baseVerts = [];
+    var lidVerts = [];
+    var holeVerts = [];
+	var slices, stacks;
+    var prev, curr;
+    var baseColor = FeatureApi.HexToColorVector("9CA175");
+    var lidColor = FeatureApi.HexToColorVector("565250");
+    var radius = 2;
+    var v4;
 
-    get VertexCount() {
-        return this.#vertexCount;
-    }
+    // Generate the base of the trash can
+    baseVerts.push(vec4(2,0,0,1));
+    baseVerts.push(vec4(2,3,0,1));
 
-    Generate() {
-        this.#DrawTrashCan();
-    }
+    // Generate the lid of the trash can
+    // generate almost quarter circle: 0 --> PI/4 
+    // points are defined from top to bottom in the XY-plane
+    slices = 24;
+    stacks = 8;
+    var sliceInc = 2*Math.PI/slices;
+    var stackInc = Math.PI/stacks;
+    for (var phi=0; phi<=Math.PI/4; phi+=stackInc) 
+       lidVerts.push(vec4(radius*Math.cos(phi), radius*Math.sin(phi) + 3, 0, 1));
 
-    //trash can initial 2d line points for surface of revolution
-    //The points are defined from bottom up in the X-Y plane
-    #IntialPoints() {
-        var SIZE= 100; // slices
-        var outterCenter = [0,3];
-        var outterRadius = 2;    
-	    var angle = 2*Math.PI/SIZE;
+    // Generate the hole of the trash can
+    
+    // Generate the quads of the trash can
+    prev = baseVerts;
+    slices = 24;
+    stacks = 1;
 
-        // The Can points
-        this.#trashCanPoints.push([outterRadius,0,0]);
-        this.#trashCanPoints.push([outterRadius,3,0]);
 
-        // The lid of the trash can
-        for (var i = 0; i < SIZE/4; i++){
-            this.#trashCanPoints.push(vec2(outterCenter[0]+outterRadius*Math.cos(i*angle), outterCenter[1]+outterRadius*Math.sin(i*angle)));
-        }
-    }
-
-    #CreateMatrices(s, r, t) {
-        return {
-            s: FeatureApi.scale4(s[0], s[1], s[2]),
-            r: rotate(r[0], r[1], r[2], r[3]),
-            t: translate(t[0], t[1], t[2]),
-        };
-    }
-
-    //Sets up the vertices array so the trashCan can be drawn
-    #DrawTrashCan() {
-        var baseColor = FeatureApi.HexToColorVector("9CA175");
-        var lidColor = FeatureApi.HexToColorVector("565250");
-
-	    //Setup initial points matrix
-	    for (var i = 0; i<25; i++)
-	    {
-	    	this.#vertices.push(vec4(this.#trashCanPoints[i][0], this.#trashCanPoints[i][1], 
-                                       this.#trashCanPoints[i][2], 1));
-	    }
-
-	    var r;
-            var t=Math.PI/12;
-
-            // sweep the original curve another "angle" degree
-	    for (var j = 0; j < 24; j++)
-	    {
-                    var angle = (j+1)*t; 
-
-                    // for each sweeping step, generate 25 new points corresponding to the original points
-	    	for(var i = 0; i < 25 ; i++ )
-	    	{	
-	    	        r = this.#vertices[i][0];
-                            this.#vertices.push(vec4(r*Math.cos(angle), this.#vertices[i][1], -r*Math.sin(angle), 1));
-	    	}    	
-	    }
-
-           var N=52; 
-           // quad strips are formed slice by slice (not layer by layer)
-           //          ith slice      (i+1)th slice
-           //            i*N+(j+1)-----(i+1)*N+(j+1)
-           //               |              |
-           //               |              |
-           //            i*N+j --------(i+1)*N+j
-           // define each quad in counter-clockwise rotation of the vertices
-           for (var i=0; i<24; i++) // slices
-           {
-                for (var j=0; j<2; j++)  // layers
-                {
-                     FeatureApi.Quad(this.#vertices[i*N+j], this.#vertices[(i+1)*N+j], this.#vertices[(i+1)*N+(j+1)], this.#vertices[i*N+(j+1)], baseColor);
-                     this.#vertexCount += 6; 
-                } 
-               for (var j=2; j<27; j++)  // layers
-               {
-	    			FeatureApi.Quad(this.#vertices[i*N+j], this.#vertices[(i+1)*N+j], this.#vertices[(i+1)*N+(j+1)], this.#vertices[i*N+(j+1)], lidColor);
-                    this.#vertexCount += 6; 
-               }
-           }
+    // rotate around y axis
+    var m=rotate(360/slices, 0, 1, 0);
+    for (var i=0; i<slices; i++) {
+        
+        curr=[];
+        // compute the new set of points with one rotation
+        for (var j=0; j<=stacks; j++) {
+            v4 = multiply(m, prev[j]);
+            curr.push( v4 );
         }
 
-    Render(drawCount) {
-        var matrices;
-
-        modelViewStack.push(modelViewMatrix);
-        matrices = this.#CreateMatrices([1,1,1],[0,0,1,0],[0,0,0]);
-        modelViewMatrix = mult(mult(mult(mult(modelViewMatrix),this.#translationMatrix, matrices.t), matrices.r), matrices.s);
-        gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
-        gl.drawArrays(gl.TRIANGLES, drawCount, this.#vertexCount);
-        modelViewMatrix = modelViewStack.pop();
+        // create the quads(triangles) for this slice
+        //         ith slice      (i+1)th slice
+        //           prev[j] ------ curr[j]
+        //             |               |
+        //             |               |
+        //           prev[j+1] ---  curr[j+1]
+        // each quad is defined with points specified in counter-clockwise rotation
+        for (var j=0; j<stacks; j++) { 
+            FeatureApi.Quad(prev[j], prev[j+1], curr[j+1], curr[j],baseColor);
+            numVertTrashCan += 6;
+        }	
+        prev = curr;  
     }
+
+    prev = lidVerts;
+    slices = 24;
+    stacks = 8;
+
+    // rotate around y axis
+    m=rotate(360/slices, 0, 1, 0);
+    for (var i=0; i<slices; i++) {
+        
+        curr=[];
+        // compute the new set of points with one rotation
+        for (var j=0; j<=stacks; j++) {
+            v4 = multiply(m, prev[j]);
+            //console.log(v4);
+
+            curr.push( v4 );
+            //console.log(curr);
+
+        }
+
+        // create the quads(triangles) for this slice
+        //         ith slice      (i+1)th slice
+        //           prev[j] ------ curr[j]
+        //             |               |
+        //             |               |
+        //           prev[j+1] ---  curr[j+1]
+        // each quad is defined with points specified in counter-clockwise rotation
+        for (var j=0; j<stacks; j++) { 
+            //console.log(curr);
+            FeatureApi.Quad(prev[j], prev[j+1], curr[j+1], curr[j],lidColor);
+            numVertTrashCan += 6;
+        }
+        prev = curr;  
+    }
+}
+
+var RenderTrashCan = function()
+{           
+    modelViewStack.push(modelViewMatrix);
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+    gl.drawArrays(gl.TRIANGLES, 0 , numVertTrashCan);
+    modelViewMatrix = modelViewStack.pop();
+}
+
+// a 4x4 matrix multiple by a vec4
+function multiply(m, v)
+{
+    var vv=vec4(
+     m[0][0]*v[0] + m[0][1]*v[1] + m[0][2]*v[2]+ m[0][3]*v[3],
+     m[1][0]*v[0] + m[1][1]*v[1] + m[1][2]*v[2]+ m[1][3]*v[3],
+     m[2][0]*v[0] + m[2][1]*v[1] + m[2][2]*v[2]+ m[2][3]*v[3],
+     1);
+    return vv;
 }
